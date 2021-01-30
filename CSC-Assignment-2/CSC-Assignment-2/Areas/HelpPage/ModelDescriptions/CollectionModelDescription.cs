b66a5a4ae425d2d8fda72347d5dd5a5f61630bb5
matthiaslim-01 <matthiaslim.18@ichat.sp.edu.sg>
2021-01-30 19:32:00 +0800

    dotnet to dotnet core conversion

--- CSC-Assignment-2/CSC-Assignment-2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CSC_Assignment_2.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- CSC-Assignment-2/CSC-Assignment-2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CSC_Assignment_2.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
