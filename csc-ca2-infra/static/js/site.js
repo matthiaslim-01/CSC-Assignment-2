b1128eeee76a91b0c5fc03300591a225b3b5b2d8
Ruri Ichigyo <dteh69@gmail.com>
2021-02-07 11:33:49 +0800

    add search

--- csc-ca2-infra/static/js/site.js
+++ csc-ca2-infra/static/js/site.js
@@ -35,3 +35,39 @@ $("#submitTalent").click((e) => {
         $result.text("Check your inputs.")
     }
 });
+
+//SEARCH Method
+$('#search').keyup(function () {
+    //get data from json file
+    //var urlForJson = "data.json";
+
+    //get data from Restful web Service in development environment
+    var urlForJson = "https://ab4z15tt79.execute-api.us-east-1.amazonaws.com/dev/api/getAllTalents";
+
+    //Url for the Cloud image hosting
+    var urlForCloudImage = "https://csc-assignment-photo-bucket-teh.s3.amazonaws.com/";
+
+    var searchField = $('#search').val();
+    var myExp = new RegExp(searchField, "i");
+    $.getJSON(urlForJson, function (data) {
+        var output = '<ul class="searchresults">';
+        $.each(data, function (key, val) {
+            //for debug
+            console.log(data);
+            if ((val.Name.search(myExp) != -1) ||
+			(val.Bio.search(myExp) != -1)) {
+                output += '<li>';
+                output += '<h2>' + val.Name + '</h2>';
+                //get the absolute path for local image
+                //output += '<img src="images/'+ val.ShortName +'_tn.jpg" alt="'+ val.Name +'" />';
+
+                //get the image from cloud hosting
+                output += '<img src=' + urlForCloudImage + val.ShortName + "_tn.jpg alt=" + val.Name + '" />';
+                output += '<p>' + val.Bio + '</p>';
+                output += '</li>';
+            }
+        });
+        output += '</ul>';
+        $('#update').html(output);
+    }); //get JSON
+});
